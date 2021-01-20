8845bd76f7db932e5eb03768ee39cdac4d1ee200
ninesky00 <jiangzho@gmail.com>
2021-01-20 11:27:33 -0600

    add passenger_names method to Flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,9 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
+  
+  def passenger_names
+    passengers.pluck(:name)
+  end
+  
 end
