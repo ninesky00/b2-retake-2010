af5b0208beafb223fbe173903b0fc9f2a1949b30
ninesky00 <jiangzho@gmail.com>
2021-01-20 11:35:21 -0600

    add feature test to display the names of all passenger for each flight

---
+++ spec/features/flights/index_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "flights index page" do 
+  before (:each) do 
+    @delta = Airline.create!(name: "delta")
+    @flight1 = Flight.create!(number:1337, date:"2015-10-06", departure_city:"houston", arrival_city:"denver", airline:@delta)
+    @flight2 = Flight.create!(number:7331, date:"2015-10-07", departure_city:"los angeles", arrival_city:"new york", airline:@delta)
+    @passenger1 = Passenger.create!(name: "joe", age: 20)
+    @passenger2 = Passenger.create!(name: "emily", age: 21)
+    @passenger3 = Passenger.create!(name: "jane", age: 22)
+    @passenger4 = Passenger.create!(name: "eric", age: 23)
+    FlightPassenger.create!(flight: @flight1, passenger: @passenger1)
+    FlightPassenger.create!(flight: @flight1, passenger: @passenger2)
+    FlightPassenger.create!(flight: @flight2, passenger: @passenger3)
+    FlightPassenger.create!(flight: @flight2, passenger: @passenger4)
+    visit flights_path
+  end
+
+  it "display a list of all flight numbers" do 
+    within("#flight-#{@flight1.id}") do 
+      expect(page).to have_content(1337)
+    end
+
+    within("#flight-#{@flight2.id}") do 
+      expect(page).to have_content(7331)
+    end
+  end
+
+  it "under each flight number, it display the names of all that flight's passengers" do 
+    within("#flight-#{@flight1.id}") do 
+      expect(page).to have_content(@passenger1.name)
+      expect(page).to have_content(@passenger2.name)
+    end
+
+    within("#flight-#{@flight2.id}") do 
+      expect(page).to have_content(@passenger3.name)
+      expect(page).to have_content(@passenger4.name)
+    end
+  end
+end
